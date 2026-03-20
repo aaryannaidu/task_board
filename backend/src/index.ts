50e3e6a13e4374404156e545db7d49067917de38
Naman Kumar <namancomk@gmail.com>
2026-03-20 22:17:18 +0530

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,8 +16,8 @@ app.use(cookieParser());
 app.use('/api/auth', require('./routes/auth').default);
 app.use('/api/projects', authenticate, require('./routes/project_router').default);
 app.use('/api/projects/:projectid/boards',authenticate,require('./routes/board_router').default);
-app.use('/api/projects/:projectid/tasks',authenticate,require('./router/task_router').default);
-app.use('/api/projects/:projectid/tasks/:taskid/comments',authenticate,require('./router/comment_router').default);
+app.use('/api/projects/:projectid/tasks',authenticate,require('./routes/task_router').default);
+app.use('/api/projects/:projectid/tasks/:taskid/comments',authenticate,require('./routes/comment_router').default);
 app.use('/api/notifications',authenticate,require('./routes/notification_router').default);
 
 app.get('/', (_req: Request, res: Response) => {
